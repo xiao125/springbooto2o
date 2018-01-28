031359dd3f195b6ef3fdd3c1cccf9b79edbaef2a
xixi <caixi.tai@qq.com>
2018-01-28 22:57:40 +0800

    UserProductMapService

---
+++ src/main/java/com/imooc/o2o/enums/UserProductMapStateEnum.java
@@ -0,0 +1,39 @@
+package com.imooc.o2o.enums;
+
+/**
+ * Created by Administrator on 2018/1/28.
+ */
+public enum  UserProductMapStateEnum {
+
+    SUCCESS(1, "操作成功"),
+    INNER_ERROR(-1001, "操作失败"),
+    NULL_USERPRODUCT_ID(-1002,"UserProductId为空"),
+    NULL_USERPRODUCT_INFO(-1003,"传入了空的信息");
+
+    private int state;
+
+    private String stateInfo;
+
+
+    UserProductMapStateEnum(int state, String stateInfo) {
+        this.state = state;
+        this.stateInfo = stateInfo;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    public static UserProductMapStateEnum stateOf(int index){
+        for (UserProductMapStateEnum state : values()){
+            if (state.getState() == index){
+                return state;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/imooc/o2o/service/UserProductMapService.java
@@ -0,0 +1,11 @@
+package com.imooc.o2o.service;
+
+/**
+ * Created by Administrator on 2018/1/28.
+ */
+public interface UserProductMapService  {
+
+
+
+
+}
